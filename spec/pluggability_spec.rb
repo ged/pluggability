cd4bf4db6bfd83512f2258737f84211e0d0392d5
Michael Granger <ged@FaerieMUD.org>
2012-08-03 11:33:38 -0700

    Finish up conversion to Pluggability

--- spec/pluggability_spec.rb
+++ spec/pluggability_spec.rb
@@ -16,12 +16,23 @@ require 'pluggability'
 
 require 'spec/lib/helpers'
 
+class Plugin
+	extend Pluggability
+	plugin_prefixes 'plugins', 'plugins/private'
+end
+
+class SubPlugin < Plugin; end
+class TestingPlugin < Plugin; end
+class BlackSheep < Plugin; end
+module Test
+	class LoadablePlugin < Plugin; end
+end
+
 
 describe Pluggability do
 
 	before( :each ) do
-		setup_logging( :fitharn )
-		Loggability.level = :debug
+		setup_logging( :fatal )
 	end
 
 	after( :each ) do
@@ -30,58 +41,53 @@ describe Pluggability do
 
 
 	it "allows extended objects to declare one or more prefixes to use when requiring derviatives" do
-		pluginclass = Class.new
-		pluginclass.extend( Pluggability )
-		pluginclass.plugin_prefixes "plugins", "plugins/private"
-		pluginclass.plugin_prefixes.should == ["plugins", "plugins/private"]
+		Plugin.plugin_prefixes.should == ['plugins', 'plugins/private']
 	end
+
 
 
 	context "-extended class" do
 
-		before( :all ) do
-			@plugin = Class.new do
-				def name; "Plugin"; end
-				extend Pluggability
-				plugin_prefixes "plugins", "plugins/private"
-			end
-			@subplugin = Class.new( @plugin ) { def name; "SubPlugin"; end }
-		end
-
-
 		it "knows about all of its derivatives" do
-			@plugin.derivatives.keys.should include( 'sub' )
-			@plugin.derivatives.keys.should include( 'subplugin' )
-			@plugin.derivatives.keys.should include( 'SubPlugin' )
-			@plugin.derivatives.keys.should include( @subplugin )
+			Plugin.derivatives.keys.should include( 'sub' )
+			Plugin.derivatives.keys.should include( 'subplugin' )
+			Plugin.derivatives.keys.should include( 'SubPlugin' )
+			Plugin.derivatives.keys.should include( SubPlugin )
 		end
 
 		it "returns derivatives directly if they're already loaded" do
-			class AlreadyLoadedPlugin < @plugin; end
+			class AlreadyLoadedPlugin < Plugin; end
 			Kernel.should_not_receive( :require )
-			@plugin.create( 'alreadyloaded' ).should be_an_instance_of( AlreadyLoadedPlugin )
-			@plugin.create( 'AlreadyLoaded' ).should be_an_instance_of( AlreadyLoadedPlugin )
-			@plugin.create( 'AlreadyLoadedPlugin' ).should be_an_instance_of( AlreadyLoadedPlugin )
-			@plugin.create( AlreadyLoadedPlugin ).should be_an_instance_of( AlreadyLoadedPlugin )
+			Plugin.create( 'alreadyloaded' ).should be_an_instance_of( AlreadyLoadedPlugin )
+			Plugin.create( 'AlreadyLoaded' ).should be_an_instance_of( AlreadyLoadedPlugin )
+			Plugin.create( 'AlreadyLoadedPlugin' ).should be_an_instance_of( AlreadyLoadedPlugin )
+			Plugin.create( AlreadyLoadedPlugin ).should be_an_instance_of( AlreadyLoadedPlugin )
 		end
 
 		it "filters errors that happen when creating an instance of derivatives so they " +
 			"point to the right place" do
-			class PugilistPlugin < @plugin
+			class PugilistPlugin < Plugin
 				def initialize
 					raise "Oh noes -- an error!"
 				end
 			end
 
-			expect {
-				@plugin.create('pugilist')
-			}.to raise_error( RuntimeError, /#{__FILE__}/ )
+			exception = nil
+			begin
+				Plugin.create('pugilist')
+			rescue ::RuntimeError => err
+				exception = err
+			else
+				fail "Expected an exception to be raised."
+			end
+
+			exception.backtrace.first.should =~ /#{__FILE__}/
 		end
 
 		it "will refuse to create an object other than one of its derivatives" do
 			class Doppelgaenger; end
 			expect {
-				@plugin.create(Doppelgaenger)
+				Plugin.create(Doppelgaenger)
 			}.to raise_error( ArgumentError, /is not a descendent of/ )
 		end
 
@@ -89,14 +95,14 @@ describe Pluggability do
 		it "will load new plugins from the require path if they're not loaded yet" do
 			loaded_class = nil
 
-			@plugin.should_receive( :require ).with( 'plugins/dazzle_plugin' ).and_return do |*args|
-				loaded_class = Class.new( @plugin )
+			Plugin.should_receive( :require ).with( 'plugins/dazzle_plugin' ).and_return do |*args|
+				loaded_class = Class.new( Plugin )
 				# Simulate a named class, since we're not really requiring
-				@plugin.derivatives['dazzle'] = loaded_class 
+				Plugin.derivatives['dazzle'] = loaded_class 
 				true
 			end
 
-			@plugin.create( 'dazzle' ).should be_an_instance_of( loaded_class )
+			Plugin.create( 'dazzle' ).should be_an_instance_of( loaded_class )
 		end
 
 
@@ -104,23 +110,23 @@ describe Pluggability do
 			"derivative fails" do
 
 			# at least 6 -> 3 variants * 2 paths
-			@plugin.should_receive( :require ).
+			Plugin.should_receive( :require ).
 				at_least(6).times.
 				and_return {|path| raise LoadError, "path" }
 
 			expect {
-				@plugin.create('scintillating')
+				Plugin.create('scintillating')
 			}.to raise_error( FactoryError, /couldn't find a \S+ named \S+.*tried \[/i )
 		end
 
 
 		it "will output a sensible description when a require succeeds, but it loads something unintended" do
 			# at least 6 -> 3 variants * 2 paths
-			@plugin.should_receive( :require ).and_return( true )
+			Plugin.should_receive( :require ).and_return( true )
 
 			expect {
-				@plugin.create('corruscating')
-			}.to raise_error( FactoryError, /Require of '\S+' succeeded, but didn't load a @plugin/i )
+				Plugin.create('corruscating')
+			}.to raise_error( FactoryError, /Require of '\S+' succeeded, but didn't load a Plugin/i )
 		end
 
 
@@ -128,13 +134,30 @@ describe Pluggability do
 			"derivative if none of the paths work" do
 
 			# at least 6 -> 3 variants * 2 paths
-			@plugin.should_receive( :require ).at_least(6).times.and_return {|path|
+			Plugin.should_receive( :require ).at_least(6).times.and_return {|path|
 				raise ScriptError, "error while parsing #{path}"
 			}
 
 			expect {
-				@plugin.create('portable')
+				Plugin.create('portable')
 			}.to raise_error( ScriptError, /error while parsing/ )
+		end
+
+
+		it "can preload all of its derivatives" do
+			Gem.should_receive( :find_files ).with( 'plugins/*.rb' ).
+				and_return([ 'plugins/first.rb' ])
+			Gem.should_receive( :find_files ).with( 'plugins/private/*.rb' ).
+				and_return([ 'plugins/private/second.rb', 'plugins/private/third.rb' ])
+
+			Plugin.should_receive( :require ).with( 'plugins/first.rb' ).
+				and_return( true )
+			Plugin.should_receive( :require ).with( 'plugins/private/second.rb' ).
+				and_return( true )
+			Plugin.should_receive( :require ).with( 'plugins/private/third.rb' ).
+				and_return( true )
+			
+			Plugin.load_all
 		end
 	end
 
@@ -142,7 +165,7 @@ describe Pluggability do
 	context "derivative of an extended class" do
 
 		it "knows what type of factory loads it" do
-			TestingPlugin.factory_type.should == '@plugin'
+			TestingPlugin.factory_type.should == 'Plugin'
 		end
 
 		it "raises a FactoryError if it can't figure out what type of factory loads it" do
@@ -154,10 +177,10 @@ describe Pluggability do
 	end
 
 
-	context "derivative of an extended class that isn't named <Something>@plugin" do
+	context "derivative of an extended class that isn't named <Something>Plugin" do
 
 		it "is still creatable via its full name" do
-			@plugin.create( 'blacksheep' ).should be_an_instance_of( BlackSheep )
+			Plugin.create( 'blacksheep' ).should be_an_instance_of( BlackSheep )
 		end
 
 	end
@@ -166,7 +189,7 @@ describe Pluggability do
 	context "derivative of an extended class in another namespace" do
 
 		it "is still creatable via its derivative name" do
-			@plugin.create( 'loadable' ).should be_an_instance_of( Test::LoadablePlugin )
+			Plugin.create( 'loadable' ).should be_an_instance_of( Test::LoadablePlugin )
 		end
 
 	end
