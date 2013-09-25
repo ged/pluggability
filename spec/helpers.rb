85612c23244416269405c9180757e8d9cd107d4d
Michael Granger <ged@FaerieMUD.org>
2013-09-25 16:36:04 -0700

    Converted to use new RSpec syntax.

---
+++ spec/helpers.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/ruby
+# coding: utf-8
+
+require 'rspec'
+require 'loggability/spechelpers'
+require 'pluggability'
+
+### Mock with Rspec
+RSpec.configure do |config|
+
+	config.treat_symbols_as_metadata_keys_with_true_values = true
+	config.run_all_when_everything_filtered = true
+	config.filter_run :focus
+	config.order = 'random'
+	config.mock_with( :rspec ) do |mock_config|
+		mock_config.syntax = :expect
+	end
+
+	config.include( Loggability::SpecHelpers )
+end
+
+# vim: set nosta noet ts=4 sw=4:
+
